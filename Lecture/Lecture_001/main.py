675dd69dd1e1b84f1d5c296745b53ea3390878b4
Dvortsov Maksim <maximgsm@gmail.com>
2023-05-25 22:12:33 +0700

    Лекция 1 цикл for & array

--- Lecture/Lecture_001/main.py
+++ Lecture/Lecture_001/main.py
@@ -1,5 +1,9 @@
 # a = int(input('Введите первое число: '))
 # b = int(input('Введите второе число: '))
 # print(a+b)
-a = 1 > 4
-print(a)
+# a = 1 > 4
+# print(a)
+a = 'qwert'
+#print(a[3])
+for i in a:
+    print(i)
